00e6c736668a5d34de33c057abc74dca938f6bf0
Ankit patel <28822999+AAnkitpatel@users.noreply.github.com>
2021-09-02 13:40:53 +0530

    Update disk-check.sh

--- disk-check.sh
+++ disk-check.sh
@@ -2,8 +2,8 @@ for i in $(cat server-info)
 do
 	ip=$(echo $i | cut -d "=" -f1)
 	name=$(echo $i | cut -d "=" -f2)
-	output=$(ssh -l w3villa $ip -C '/usr/lib/nagios/plugins/check_disk -w 20% -c 10% -p /  && df -h /')
-	log=$(ssh -l w3villa $ip -C ' du -sh /home/w3villa/apfusion-rails/current/log/; du -sh /var/log 2>/dev/null')
+	output=$(ssh -l user $ip -C '/usr/lib/nagios/plugins/check_disk -w 20% -c 10% -p /  && df -h /')
+	log=$(ssh -l user $ip -C ' du -sh /home/user/app-rails/current/log/; du -sh /var/log 2>/dev/null')
 	#echo "$ip = $name"
 	echo  "\n$name===>\n $output" 
 	echo "\n $log"
