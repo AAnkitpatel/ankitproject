8718089c108891ecefac455140b460bcb3d3235e
Ankit patel <28822999+AAnkitpatel@users.noreply.github.com>
2021-09-02 13:39:22 +0530

    Update logs-cleaner.sh

--- logs-cleaner.sh
+++ logs-cleaner.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
-sync_apfusion="44.227.24.74=sync-apfusion"
-staging="35.162.154.176=staging"       
-APF_prod="35.167.65.242=APF-prod"       
-sidekiq="44.233.192.168=sidekiq"       
-apfusion_analytics="35.167.146.144=apfusion-analytics"       
-sync_apfusion_2="34.215.103.235=sync-apfusion_2"    
-sync_apfusion_3="34.212.50.161=sync-apfusion_3"    
 
 user="w3villa"
 log_dir1="/var/www/html"
-main_log="/home/w3villa/apfusion-rails/current/log/"
+main_log="/home/w3villa/app-path/current/log/"
 
-for i in  $APF_prod $sync_apfusion $sync_apfusion_2 $sync_apfusion_3 $sidekiq $staging $apfusion_analytics
+for i in  $prod $sync_ $sync_2 $sync__3 $sidekiq $staging $analytics
 do
 ip=$(echo $i | cut -d "=" -f1)
 name=$(echo $i |cut -d "=" -f2)
